ea9b6dea45484f1c3d9ce27aabff06403d3be4c0
haeunNam <nhe0103@gamail.com>
2024-02-10 23:30:13 +0900

    second

--- main.js
+++ main.js
@@ -3,34 +3,6 @@
 // 다만, 0~1 사이의 숫자가 출력되므로 랜덤한 두자리 수를 뽑을 때에는 *100을 해줌
 // Math.random()*100 : 1~99까지 출력, +1을 해줘야 1~100까지 출력
 // ☑️ Math.floor() -> 반올림 함수
-
-let computerNum = 0;
-let playButton =document.getElementById("play-button");
-let userInput = document.getElementById("user-input")
-let resultArea = document.getElementById("result-area")
-
-playButton.addEventListener("click",play)
-
-function pickRandomNum() {
-    computerNum=Math.floor(Math.random()*100+1);
-    console.log("정답",computerNum);
-}
-
-function play(params) {
-    let userValue=userInput.value;
-
-    if(userValue<computerNum){
-        resultArea.textContent="UP"
-        
-    } else if(userValue>computerNum){
-        resultArea.textContent="Down!"
-        
-    } else{
-        resultArea.textContent="정답!"
-    }
-}
-pickRandomNum();
-
 // 2. 유저가 번호를 입력한다. 그리고 go라는 버튼을 누름
 // 만약에 유저가 랜덤번호를 맞추면, 맞췄습니다!
 // 랜덤번호 < 유저 번호 DOWN!!
@@ -39,3 +11,75 @@ pickRandomNum();
 // 5번의 기회를 다 쓰면 게임이 끝난다(더이상 추측 불가, 버튼이 disable )
 // 유저가 1~100 범위 밖의 숫자를 입력하면 알려준다. (기회를 깎지 않는다)
 // 유저가 이미 입력한 숫자를 또 입력하면, 알려준다. (기회를 깎지 않늗다)
+
+let computerNum = 0;
+let playButton = document.getElementById("play-button");
+let userInput = document.getElementById("user-input");
+let resultArea = document.getElementById("result-area");
+let resetButton = document.getElementById("reset-button");
+let chances = 5;
+let gameOver = false;
+let chanceArea = document.getElementById("chance-area")
+let history=[]
+
+
+playButton.addEventListener("click",play)
+resetButton.addEventListener("click",reset)
+userInput.addEventListener("focus",function() {userInput.value=""})
+
+
+function pickRandomNum() {
+    computerNum=Math.floor(Math.random()*100+1);
+    console.log("정답",computerNum);
+}
+
+function play() {
+    let userValue=userInput.value;
+
+    if(userValue<1||userValue>100){
+        resultArea.textContent="1과 100사이의 숫자를 입력해주세요"
+        return; // return만 쓰면 함수를 실행하지 않고 종료
+    }
+
+    if(history.includes(userValue)){
+        resultArea.textContent="이미 입력한 숫자입니다 다른 숫자를 입력해주세요"
+        return;
+    }
+
+    chances--;
+    chanceArea.textContent=`남은 기회는 ${chances}번` 
+    console.log("chance",chances);
+
+    if(userValue<computerNum){
+        resultArea.textContent="UP"       
+    } else if(userValue>computerNum){
+        resultArea.textContent="Down!"
+        
+    } else{
+        resultArea.textContent="정답!"
+        gameOver=true
+        // playButton.disabled=true
+        // resetButton.disabled=true
+    }
+
+    history.push(userValue)
+    console.log(history);
+
+    if(chances<1){
+        gameOver=true;
+    }
+    if(gameOver==true){
+        playButton.disabled=true;
+    }
+}
+
+function reset(params) {
+    // user input창이 깨끗하게 정리되고
+    userInput.value=""
+    // 새로운 번호가 생성되고
+    pickRandomNum()
+    
+    resultArea.textContent="다시 시작~~"
+}
+
+pickRandomNum();
